1b894284876982d57238a87d07eedf0299c4a1a0
kosumi <eugeneq2632@gmail.com>
2024-06-26 18:36:22 +0800

    1.5

--- lib/pages/factory1.dart
+++ lib/pages/factory1.dart
@@ -51,9 +51,6 @@ class _FactoryClass1State extends State<FactoryClass1> {
   @override
   Widget build(BuildContext context) {
 
-  double screenWidth = MediaQuery.of(context).size.width;
-  double screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Factory 1"),
